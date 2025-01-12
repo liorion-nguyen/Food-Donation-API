dc56c989f6b0a25449dee706b4590c4eb2b71bdd
LiorionNguyen <liorion.nguyen@gmail.com>
2025-01-12 23:18:19 +0700

    update

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,16 @@ import * as cookieParser from 'cookie-parser';
 import { SocketAdapter } from './SocketAdapter';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {cors: true});
+  const app = await NestFactory.create(AppModule, { cors: true });
+  
+  // Cấu hình WebSocket Adapter
   app.useWebSocketAdapter(new SocketAdapter(app));
+  
+  // Sử dụng cookie-parser chỉ một lần
   app.use(cookieParser());
-  await app.listen(process.env.PORT, '0.0.0.0');
+
+  // Nghe trên tất cả các địa chỉ IP
+  await app.listen(process.env.PORT || 3000, '0.0.0.0');
 }
+
 bootstrap();
